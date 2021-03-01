af119c816c010fc216fee6e6bbfbe7e36c96a1ae
Adphi <philippe.adrien.nousse@gmail.com>
2021-03-01 19:09:15 +0100

    add missing kubebuilder rbac annotations

--- controllers/service_controller.go
+++ controllers/service_controller.go
@@ -45,6 +45,9 @@ type ServiceReconciler struct {
 
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;update;patch
 // +kubebuilder:rbac:groups=core,resources=services/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core,resources=endpoints,verbs=get;list;watch
+// +kubebuilder:rbac:groups=core,resources=nodes,verbs=get;list;watch
+// +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
 func (r *ServiceReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
